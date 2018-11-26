298bea5ed2b71da7cc16842ad870290f5a8cb5df
Tristan-R <39279889+Tristan-R@users.noreply.github.com>
2018-11-26 23:39:04 +0000

    Make HumanPlayer class the handler for player input.

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * Runs the game with a human player and contains code needed to read inputs.
  *
@@ -10,6 +12,26 @@ public class HumanPlayer {
      * return : A string containing the input the player entered.
      */
     protected String getInputFromConsole() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            while (true) {
+                String command = reader.readLine();
+
+                if (command == null) {
+                    System.exit(0);
+
+                }
+
+                return this.getNextAction(command);
+
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+
+        }
+
         return null;
     }
 
@@ -19,7 +41,7 @@ public class HumanPlayer {
      *
      * @return : Processed output or Invalid if the @param command is wrong.
      */
-    protected String getNextAction() {
+    protected String getNextAction(String command) {
         return null;
     }
 
