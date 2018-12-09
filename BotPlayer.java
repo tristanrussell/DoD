54b4d55d2cf0db17ed4717f05a60c6248cabd0c7
Tristan-R <39279889+Tristan-R@users.noreply.github.com>
2018-12-09 21:27:13 +0000

    Make the bot chase the player when it sees the player.

--- BotPlayer.java
+++ BotPlayer.java
@@ -9,8 +9,15 @@ public class BotPlayer {
     /* Local map around bot */
     private char[][] mapMemory;
 
+    private boolean toUpdate = true;
+
     /* The position of the bot relative to the map in mapMemory */
     private int[] savedPosition = new int[2];
+
+    private boolean isPlayer = false;
+
+    /* Coordinates of the player if they are found */
+    private int[] playerPosition = new int[2];
 
     /**
      * Default constructor.
@@ -39,7 +46,8 @@ public class BotPlayer {
      * @return : The next move to make.
      */
     protected String nextMove() {
-        if (savedPosition[0] == 0 || savedPosition[0] == 4 || savedPosition[1] == 0 || savedPosition[1] == 4) {
+        if (toUpdate) {
+            toUpdate = false;
             return "LOOK";
 
         }
@@ -52,19 +60,20 @@ public class BotPlayer {
      *
      * @param map : The current map object being used.
      */
-    protected void takeTurn(Map map) {
-        boolean result;
+    protected void takeTurn(Map map, PlayerInfo player) {
+        boolean hasMoved;
         switch (this.nextMove()) {
             case "LOOK":
                 savedPosition[0] = 2;
                 savedPosition[1] = 2;
                 mapMemory = map.getLocalMap(botPosition[0], botPosition[1]);
+                isPlayer = checkForPlayer(player);
                 break;
 
             case "MOVE":
                 do {
-                    result = move(map, randDirection());
-                } while(!result);
+                    hasMoved = move(map, randDirection());
+                } while(!hasMoved);
                 break;
 
             default:
@@ -73,6 +82,21 @@ public class BotPlayer {
 
         // DEBUGGING
         System.out.println(Arrays.toString(botPosition));
+    }
+
+    protected boolean checkForPlayer(PlayerInfo player) {
+        int[] playerPos = player.getPlayerPosition();
+        int testY = Math.abs(playerPos[0] - botPosition[0]);
+        int testX = Math.abs(playerPos[1] - botPosition[1]);
+
+        if (testX < 3 && testY < 3) {
+            playerPosition[0] = 2 + playerPos[0] - botPosition[0];
+            playerPosition[1] = 2 + playerPos[1] - botPosition[1];
+            return true;
+
+        }
+        return false;
+
     }
 
     /**
@@ -93,6 +117,27 @@ public class BotPlayer {
      * @param direction : The direction that the bot should move.
      */
     protected boolean move(Map map, char direction) {
+        if (isPlayer) {
+            if(Math.abs(savedPosition[0] - playerPosition[0]) > Math.abs(savedPosition[1] - playerPosition[1])) {
+                if (savedPosition[0] < playerPosition[0]) {
+                    direction = 'S';
+
+
+                } else {
+                    direction = 'N';
+
+                }
+            } else {
+                if (savedPosition[1] < playerPosition[1]) {
+                    direction = 'E';
+
+
+                } else {
+                    direction = 'W';
+
+                }
+            }
+        }
         switch (direction) {
             case 'N':
                 if (mapMemory[savedPosition[0] - 1][savedPosition[1]] == '#') {
@@ -101,23 +146,30 @@ public class BotPlayer {
                 }
                 if (!map.isWall((botPosition[0] - 1), botPosition[1])) {
                     botPosition[0]--;
-                    savedPosition[0]--;
+                    if (--savedPosition[0] == 0) {
+                        if (!isPlayer) {
+                            toUpdate = true;
 
+                        }
+                    }
                 }
-                return true;
+                break;
 
             case 'E':
                 if (mapMemory[savedPosition[0]][savedPosition[1] + 1] == '#') {
                     return false;
 
-
                 }
                 if (!map.isWall(botPosition[0], (botPosition[1] + 1))) {
                     botPosition[1]++;
-                    savedPosition[1]++;
+                    if (++savedPosition[1] == 4) {
+                        if (!isPlayer) {
+                            toUpdate = true;
 
+                        }
+                    }
                 }
-                return true;
+                break;
 
             case 'S':
                 if (mapMemory[savedPosition[0] + 1][savedPosition[1]] == '#') {
@@ -126,10 +178,14 @@ public class BotPlayer {
                 }
                 if (!map.isWall((botPosition[0] + 1), botPosition[1])) {
                     botPosition[0]++;
-                    savedPosition[0]++;
+                    if (++savedPosition[0] == 4) {
+                        if (!isPlayer) {
+                            toUpdate = true;
 
+                        }
+                    }
                 }
-                return true;
+                break;
 
             case 'W':
                 if (mapMemory[savedPosition[0]][savedPosition[1] - 1] == '#') {
@@ -138,15 +194,26 @@ public class BotPlayer {
                 }
                 if (!map.isWall(botPosition[0], (botPosition[1] - 1))) {
                     botPosition[1]--;
-                    savedPosition[1]--;
+                    if (--savedPosition[1] == 0) {
+                        if (!isPlayer) {
+                            toUpdate = true;
 
+                        }
+                    }
                 }
-                return true;
+                break;
 
             default:
                 return false;
 
         }
+        if (isPlayer && playerPosition[0] == savedPosition[0] && playerPosition[1] == savedPosition[1]) {
+            isPlayer = false;
+            toUpdate = true;
+
+        }
+        return true;
+
     }
 
 }
