dcd9df40207c552b8bad301159b91bb8d877debe
Tristan-R <39279889+Tristan-R@users.noreply.github.com>
2018-12-09 15:06:05 +0000

    Comment BotPlayer file.

--- BotPlayer.java
+++ BotPlayer.java
@@ -1,14 +1,23 @@
 public class BotPlayer {
 
+    /* Action to perform in the bot's rotation of moves */
     private int turnInRotation = 1;
 
     /* Coordinates of the bot as {y, x} */
     private int[] botPosition;
 
+    /**
+     * Default constructor.
+     */
     public BotPlayer() {
 
     }
 
+    /**
+     * Sets the position of the bot.
+     *
+     * @param startPosition : The position to place the bot.
+     */
     protected void loadStartPosition(int[] startPosition) {
         botPosition = startPosition;
     }
@@ -20,6 +29,11 @@ public class BotPlayer {
         return botPosition;
     }
 
+    /**
+     * Performs the next move in the bot's rotation of moves.
+     *
+     * @param map : The current map object being used.
+     */
     protected void takeTurn(Map map) {
         switch (turnInRotation++) {
             case 1:
