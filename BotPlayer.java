87d835b765ae5c0e0cecf480f3a350f82fd4fa98
Tristan-R <39279889+Tristan-R@users.noreply.github.com>
2018-12-09 16:01:49 +0000

    Add basic bot movement.

--- BotPlayer.java
+++ BotPlayer.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class BotPlayer {
 
     /* Action to perform in the bot's rotation of moves */
@@ -30,24 +33,78 @@ public class BotPlayer {
     }
 
     /**
+     * @return : The next move in the rotation to make.
+     */
+    protected int nextMove() {
+        if (turnInRotation == 3) {
+            turnInRotation = 1;
+            return 3;
+
+        }
+        return turnInRotation++;
+
+    }
+
+    /**
      * Performs the next move in the bot's rotation of moves.
      *
      * @param map : The current map object being used.
      */
     protected void takeTurn(Map map) {
-        switch (turnInRotation++) {
+        switch (this.nextMove()) {
             case 1:
                 map.getLocalMap(botPosition[0], botPosition[1]);
                 break;
 
             case 2:
+                this.moveRandom(map);
                 break;
 
             case 3:
+                this.moveRandom(map);
                 break;
 
             default:
                 break;
+        }
+
+        System.out.println(Arrays.toString(botPosition));
+    }
+
+    protected void moveRandom(Map map) {
+        char[] directions = {'N', 'E', 'S', 'W'};
+        Random r = new Random();
+        int randDirection = r.nextInt(4);
+        this.move(map, directions[randDirection]);
+
+    }
+
+    protected void move(Map map, char direction) {
+        switch (direction) {
+            case 'N':
+                if (!map.isWall((botPosition[0] - 1), botPosition[1])) {
+                    botPosition[0]--;
+                }
+                break;
+
+            case 'E':
+                if (!map.isWall(botPosition[0], (botPosition[1] + 1))) {
+                    botPosition[1]++;
+                }
+                break;
+
+            case 'S':
+                if (!map.isWall((botPosition[0] + 1), botPosition[1])) {
+                    botPosition[0]++;
+                }
+                break;
+
+            case 'W':
+                if (!map.isWall(botPosition[0], (botPosition[1] - 1))) {
+                    botPosition[1]--;
+                }
+                break;
+
         }
     }
 
